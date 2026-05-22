efbb016583fda491375b184b852f3128080f93d2
IsaMontoya17 <isabelamontoya2021@gmail.com>
2026-05-21 19:38:30 -0500

    feat: add UnauthorizedException and ValidationException handlers to all services

---
+++ auth-service/src/main/java/com/digitalmoneyhouse/authservice/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.digitalmoneyhouse.authservice.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
