9b945e96210764d5f406bd5c6fdfbec7322bd86e
IsaMontoya17 <isabelamontoya2021@gmail.com>
2026-06-16 14:44:05 -0500

    refactor: externalize keycloak and account IDs using environment variables

--- testing-automation/src/test/java/com/digitalmoneyhouse/testing/config/TestConfig.java
+++ testing-automation/src/test/java/com/digitalmoneyhouse/testing/config/TestConfig.java
@@ -9,7 +9,9 @@ public class TestConfig {
 
     public static final String TEST_ACCOUNT_ID = "1";
     public static final String TEST_USER_ID = "1";
-    public static final String TEST_KEYCLOAK_ID = "b25c9b50-934c-4321-86c8-3782e2493550";
+    public static final String TEST_KEYCLOAK_ID = System.getenv("TEST_KEYCLOAK_ID") != null
+            ? System.getenv("TEST_KEYCLOAK_ID")
+            : "b25c9b50-934c-4321-86c8-3782e2493550";
 
     public static final String DB_URL = "jdbc:mysql://localhost:3307/digital_money_house";
     public static final String DB_USER = "root";
